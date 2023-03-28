e85160487e1ed96c904acf947ee9e7fe6d9ea4c8
Daniil <kondratenko_danil.23@mail.ru>
2023-03-28 21:54:31 +0300

    individual1

---
+++ individual1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+x = int(input("Введите день недели "))
+if x == 1:
+    print("Понедельник!")
+elif x == 2:
+    print("Вторник!")
+elif x == 3:
+    print("Среда!")
+elif x == 4:
+    print("Четверг!")
+elif x == 5:
+    print("Вторник!")
+elif x == 6:
+    print("Суббота!")
+elif x == 2:
+    print("Вторник!")
+else:
+    print("Ошибка!")
+input("Нажмите ENTER, чтобы выйти!")
